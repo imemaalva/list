6754d3716a04e0f2651512ee0dc72f8aa78f4a40
imemaalva <imelda.martinez.csc@gmail.com>
2022-02-23 14:00:26 -0600

    Cambio main

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,14 @@
+import uaslp.objetos.list.arrayList.ArrayList;
+import uaslp.objetos.list.arrayList.ArrayListIterator;
 import uaslp.objetos.list.linkedList.LinkedList;
 import uaslp.objetos.list.linkedList.LinkedListIterator;
 
 public class Main {
 
     public static void main(String[] args) {
-        LinkedList team1 = new LinkedList();
-        LinkedList team2 = new LinkedList();
-        LinkedList team3 = new LinkedList();
+        ArrayList team1 = new ArrayList();
+        ArrayList team2 = new ArrayList();
+        ArrayList team3 = new ArrayList();
 
         team1.addAtTail("Jesús");
         team1.addAtTail("Salomón");
@@ -18,7 +20,7 @@ public class Main {
 
         team3.addAtFront("Imelda");
 
-        LinkedListIterator iterator;
+        ArrayListIterator iterator;
 
         iterator = team1.getIterator();
 
